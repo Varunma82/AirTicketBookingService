14fb59765d6cf574db6bc9c6e652ed22aac2cf46
Varun Maheshwari <maheshwarivarun82@gmail.com>
2024-01-03 00:55:58 +0530

    "completed create booking"

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,9 @@
-const express=require('express')
+const express = require('express');
 
-const router=express.Router();
+const { BookingController } = require('../../controllers/index');
 
-module.exports=router;
+const router = express.Router();
+
+router.post('/bookings', BookingController.create);
+
+module.exports = router;
--- src/utils/errors/service-error.js
+++ src/utils/errors/service-error.js
@@ -4,13 +4,13 @@ class ServiceError extends Error{
     constructor(
         message='Something went wrong',
         explaination='Service layer error',
-        statusCodes=StatusCodes.INTERNAL_SERVER_ERROR
+        statusCode=StatusCodes.INTERNAL_SERVER_ERROR
     ) {
         super()
         this.name='ServiceError',
         this.message=message,
         this.explaination=explaination,
-        this.statusCodes=statusCodes
+        this.statusCode=statusCode
     }
 }
 
